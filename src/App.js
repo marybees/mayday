f862e9b371717d28bb60270d7ce29a72c5d519ab
Mary Baker <meb601@gmail.com>
2022-05-30 13:03:58 -0400

    Add component routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <>
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/contact">
+            <Contact />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/registry">
+            <Registry />
+          </Route>
+          <Route path="/directions">
+            <Directions />
+          </Route>
+          <Route path="/rsvp">
+            <Rsvp />
+          </Route>
+          <Route path="/info">
+            <Info />
           </Route>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
+      </>
     </Router>
   );
 }

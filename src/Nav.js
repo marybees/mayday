16b50162f10208515fdc60308a40a12f3376240e
Mary Baker <meb601@gmail.com>
2022-05-30 14:21:44 -0400

    Update Nav with title and font color

--- src/Nav.js
+++ src/Nav.js
@@ -3,36 +3,47 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function Nav() {
+
+  const NavTitle = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #494848;  `
+
+  const NavSubtitle = styled.h2`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #494848;
+  `
+
   const NavContainer = styled.nav`
     display: flex;
     justify-content: space-evenly;
-    background-color: gray;
-  `;
-
-  const NavLink = styled.div`
-    color: white;
-    text-decoration: none;
+    color: #494848;
   `;
 
   return (
     <>
+      <NavTitle>May Day</NavTitle>
+      <NavSubtitle>Join us for Mary & Will's Baby Bash</NavSubtitle>
       <NavContainer>
-        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>Home</div>
         </Link>
-        <Link to="/info" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/info" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>Info</div>
         </Link>
-        <Link to="/rsvp" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/rsvp" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>RSVP</div>
         </Link>
-        <Link to="/directions" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/directions" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>Directions</div>
         </Link>
-        <Link to="/registry" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/registry" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>Registry</div>
         </Link>
-        <Link to="/contact" style={{ textDecoration: 'none', color: 'white' }}>
+        <Link to="/contact" style={{ textDecoration: 'none', color: '#494848' }}>
           <div>Contact</div>
         </Link>
       </NavContainer>

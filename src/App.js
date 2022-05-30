9b3e25e0219d8531e32d83d98af0240113b4c071
Mary Baker <meb601@gmail.com>
2022-05-30 13:49:28 -0400

    Update react-router-dom to latest

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Contact from "./Contact";
+import Registry from "./Registry";
+import Directions from "./Directions";
+import Rsvp from "./Rsvp";
+import Info from "./Info";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <>
-        <Switch>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/registry">
-            <Registry />
-          </Route>
-          <Route path="/directions">
-            <Directions />
-          </Route>
-          <Route path="/rsvp">
-            <Rsvp />
-          </Route>
-          <Route path="/info">
-            <Info />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/registry" element={<Registry />}></Route>
+          <Route path="/directions" element={<Directions />}></Route>
+          <Route path="/rsvp" element={<Rsvp />}></Route>
+          <Route path="/info" element={<Info />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
+        </Routes>
       </>
     </Router>
   );

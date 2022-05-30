314d6e0f3d64d223bc6c22b04b663dabf797de19
Mary Baker <meb601@gmail.com>
2022-05-30 14:39:46 -0400

    Add font and make nav mobile responsive

--- src/Nav.js
+++ src/Nav.js
@@ -8,43 +8,59 @@ function Nav() {
     display: flex;
     justify-content: center;
     align-items: center;
-    color: #494848;  `
+    color: #494848;
+    font-family: 'Sacramento', cursive;
+    font-size: 150px;
+    line-height: 100px;
+    text-align: center;
+    font-weight: 100;
+  `
 
   const NavSubtitle = styled.h2`
     display: flex;
     justify-content: center;
+    text-align: center;
     align-items: center;
     color: #494848;
+    margin-top: -50px;
   `
 
   const NavContainer = styled.nav`
+    padding: 32px;
+    text-transform: uppercase;
     display: flex;
     justify-content: space-evenly;
     color: #494848;
+    flex-wrap: wrap;
   `;
+
+  const LinkText = styled.div`
+    padding: 4px;
+    font-weight: 600;
+  `
 
   return (
     <>
       <NavTitle>May Day</NavTitle>
-      <NavSubtitle>Join us for Mary & Will's Baby Bash</NavSubtitle>
+      <NavSubtitle>- JOIN US FOR MARY & WILL'S BABY BASH -</NavSubtitle>
       <NavContainer>
         <Link to="/" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>Home</div>
+          <LinkText>Home</LinkText>
         </Link>
         <Link to="/info" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>Info</div>
+          <LinkText>Info</LinkText>
         </Link>
         <Link to="/rsvp" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>RSVP</div>
+          <LinkText>RSVP</LinkText>
         </Link>
         <Link to="/directions" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>Directions</div>
+          <LinkText>Directions</LinkText>
         </Link>
         <Link to="/registry" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>Registry</div>
+          <LinkText>Registry</LinkText>
         </Link>
         <Link to="/contact" style={{ textDecoration: 'none', color: '#494848' }}>
-          <div>Contact</div>
+          <LinkText>Contact</LinkText>
         </Link>
       </NavContainer>
     </>
